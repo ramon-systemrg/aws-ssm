ddda12f7ea923140ae61623e3d63827a3f903480
ramon-systemrg <93551966+ramon-systemrg@users.noreply.github.com>
2021-11-01 20:23:00 -0600

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This script runs from the Vagrantfile after rebooting to register the agent
+# within the ssm console. The following three items are required. The activation
+# code and ID are generated once by setting up a hybrid activation in ssm.
+
 ACTIVATION_CODE='xxxxxxxxxxxxxxxxxxxx'
 ACTIVATION_ID='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 REGION='us-east-1'
